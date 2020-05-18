b49e41e58dddd345f8b6cfd16d6d472de7f6515e
wenjia0226 <1320073909@qq.com>
2020-05-18 11:09:08 +0800

    'swiper'

--- page/myCollection/pages/jifen/jifen.js
+++ page/myCollection/pages/jifen/jifen.js
@@ -8,16 +8,25 @@ Page({
     income: '',
     state: '',
     type: 0,
-    show: false
+    show: false,
+    currentIndex: 0,
+    currentStudentId: '',
+    childrenList: []
   },
   onShow: function (options) {
     if(wx.getStorageSync('phone')) {
-    wx.showLoading({
-      title: '加载中...',
-    })
-    let that = this;
+    this.getChildrenList(); 
+    }
+  },
+  // 获取当前学生的积分情况
+  currentStudentCode() {
+    if (this.data.currentStudentId !== '') {
+      wx.showLoading({
+        title: '加载中...',
+      })
+      let that = this;
       let url = app.globalData.URL + 'integralList', data = {
-        openId: wx.getStorageSync('openId')
+        studentId: this.data.currentStudentId
       };
       app.wxRequest(url, data, (res) => {
         that.setData({
@@ -29,6 +38,46 @@ Page({
       }, (err) => {
         console.log(err)
       })
+    }
+  },
+  getChildrenList() {
+    let that = this;
+    let url = app.globalData.URL + "childrenList", data = { openId: wx.getStorageSync('openId') };
+    //如果已经授权过
+    if (wx.getStorageSync('phone')) {
+      wx.showLoading({
+        title: '加载中...'
+      })
+      app.wxRequest(url, data, (res) => {
+        if (res.data.status == 200) {
+          that.setData({
+            childrenList: res.data.data,
+            currentStudentId: res.data.data[0].id
+          })
+          this.currentStudentCode();
+        }
+        if (res.data.status == 10220) {
+          that.setData({
+            childrenList: []
+          })
+        }
+      }, (err) => {
+        console.log(err)
+      })
+    }
+  },
+  //确定当前孩子具体是哪个
+  activeNav: function (e) {
+    let that = this;
+    this.setData({
+      currentIndex: e.detail.current
+    })
+    if (that.data.childrenList) {
+      let student = this.data.childrenList.filter((item, index) => { if (index == that.data.currentIndex) return item  });
+      that.setData({
+        currentStudentId: student[0].id
+      })
+      this.currentStudentCode();
     }
   },
   //切换
